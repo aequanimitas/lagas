0602cf5434b3b0bb0de8d752e9e93b9b75378de0
metalfatigue <htavenido@gmail.com>
2015-02-08 12:51:00 +0800

    slowly building

--- app/views.py
+++ app/views.py
@@ -1,9 +1,17 @@
 from flask import render_template
 from app import app
 
+TEMP_WORKOUTS = [{'name': 'bench press', 'max': '120lbs'}, 
+                 {'name': 'military press', 'max': '60lbs'}]
+
 @app.route('/')
 @app.route('/index')
-
 def index():
-    workouts = [{'name': 'bench press', 'max': '120lbs'}, {'name': 'military press', 'max': '60lbs'}]
-    return render_template('index.html', title='Lagas', workouts=workouts)
+    return render_template('index.html', title='Lagas', workouts=TEMP_WORKOUTS)
+
+@app.route('/workout/<name>')
+def workout(name):
+    if name in TEMP_WORKOUTS:
+        return '<h1> your workout for today is: {}'.format(name)
+    else:
+        return 'Workout not found', 404
