6ebee2103e7f17bc81c4125e0900e1fa0d1b8815
aequanimitas <htavenido@gmail.com>
2016-02-02 17:00:05 +0800

    move to server dir

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var webpack = require('webpack');
-var config = require('./webpack.config');
+var config = require('../webpack.config');
 
 var app = express();
 var compiler = webpack(config);
